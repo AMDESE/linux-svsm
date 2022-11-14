eb161185ef6f77d31f3a8ce5a4c33f4ffcd7ee63
Carlos Bilbao <carlos.bilbao@amd.com>
2022-11-14 15:02:34 +0000

    CPU: IDT: Limit handlers to #DF,#VC,#PF,#GP
    Since the SVSM doesn't handle all possible exceptions and is running with
    Restricted Injection enabled, remove all exception handlers except
    for #DF, #PF, #GP and #VC. Add comments for remaining handlers.
    
    Signed-off-by: Carlos Bilbao <carlos.bilbao@amd.com>

--- src/cpu/idt.rs
+++ src/cpu/idt.rs
@@ -19,30 +19,14 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt: InterruptDescriptorTable = InterruptDescriptorTable::new();
 
-        idt.divide_error.set_handler_fn(de_handler);
-        idt.debug.set_handler_fn(db_handler);
-        idt.non_maskable_interrupt.set_handler_fn(nmi_handler);
-        idt.breakpoint.set_handler_fn(bp_handler);
-        idt.overflow.set_handler_fn(of_handler);
-        idt.bound_range_exceeded.set_handler_fn(br_handler);
-        idt.invalid_opcode.set_handler_fn(ud_handler);
-        idt.device_not_available.set_handler_fn(nm_handler);
         unsafe {
             idt.double_fault
                 .set_handler_fn(df_handler)
                 .set_stack_index(DOUBLE_FAULT_IST as u16);
         }
-        idt.invalid_tss.set_handler_fn(ts_handler);
-        idt.segment_not_present.set_handler_fn(np_handler);
-        idt.stack_segment_fault.set_handler_fn(ss_handler);
         idt.general_protection_fault.set_handler_fn(gp_handler);
         idt.page_fault.set_handler_fn(pf_handler);
-        idt.x87_floating_point.set_handler_fn(mf_handler);
-        idt.alignment_check.set_handler_fn(ac_handler);
-        idt.machine_check.set_handler_fn(mc_handler);
-        idt.simd_floating_point.set_handler_fn(xf_handler);
         idt.vmm_communication_exception.set_handler_fn(vc_handler);
-        idt.security_exception.set_handler_fn(sx_handler);
 
         idt
     };
@@ -60,83 +44,23 @@ fn do_panic(stack_frame: InterruptStackFrame, name: &str, error_code: u64) -> ! 
     panic!("{}", msg);
 }
 
-extern "x86-interrupt" fn de_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "DE", 0)
-}
-
-extern "x86-interrupt" fn db_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "DB", 0)
-}
-
-extern "x86-interrupt" fn nmi_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "NMI", 0)
-}
-
-extern "x86-interrupt" fn bp_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "BP", 0)
-}
-
-extern "x86-interrupt" fn of_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "OF", 0)
-}
-
-extern "x86-interrupt" fn br_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "BR", 0)
-}
-
-extern "x86-interrupt" fn ud_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "UD", 0)
-}
-
-extern "x86-interrupt" fn nm_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "NM", 0)
-}
-
+/// Double fault handler
+/// Every interruption except for #PF, #VC and #GP will end up here
 extern "x86-interrupt" fn df_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
     do_panic(stack_frame, "DF", 0)
 }
 
-extern "x86-interrupt" fn ts_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    do_panic(stack_frame, "TS", error_code)
-}
-
-extern "x86-interrupt" fn np_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    do_panic(stack_frame, "NP", error_code)
-}
-
-extern "x86-interrupt" fn ss_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    do_panic(stack_frame, "SS", error_code)
-}
-
+/// General protection fault handler
 extern "x86-interrupt" fn gp_handler(stack_frame: InterruptStackFrame, error_code: u64) {
     do_panic(stack_frame, "GP", error_code)
 }
 
+/// Page fault handler
 extern "x86-interrupt" fn pf_handler(
     stack_frame: InterruptStackFrame,
     error_code: PageFaultErrorCode,
 ) {
     do_panic(stack_frame, "PF", error_code.bits())
-}
-
-extern "x86-interrupt" fn mf_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "MF", 0)
-}
-
-extern "x86-interrupt" fn ac_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    do_panic(stack_frame, "AC", error_code)
-}
-
-extern "x86-interrupt" fn mc_handler(stack_frame: InterruptStackFrame) -> ! {
-    do_panic(stack_frame, "MC", 0)
-}
-
-extern "x86-interrupt" fn xf_handler(stack_frame: InterruptStackFrame) {
-    do_panic(stack_frame, "XF", 0)
-}
-
-extern "x86-interrupt" fn sx_handler(stack_frame: InterruptStackFrame, error_code: u64) {
-    do_panic(stack_frame, "SX", error_code)
 }
 
 /// Load IDT with function handlers for each exception
