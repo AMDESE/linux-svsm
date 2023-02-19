583f2187abf06512e401e883d7b27dceeb8a1f11
Dov Murik <dovmurik@linux.ibm.com>
2023-02-19 22:00:10 +0200

    Move core protocol implementation to protocols/core.rs
    The switch function 'handle_request' is split between svsm_request.rs
    (selecting the protocol) and protocols/core.rs (selecting the call).
    
    No functional change intended.
    
    Signed-off-by: Dov Murik <dovmurik@linux.ibm.com>

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -5,5 +5,12 @@
  * Authors: Dov Murik <dovmurik@linux.ibm.com>
  */
 
+/// Implementation of the core protocol (0)
+pub mod core;
 /// Error codes returned from the SVSM calls
 pub mod error_codes;
+
+pub use crate::protocols::core::*;
+
+/// 0
+pub const SVSM_CORE_PROTOCOL: u32 = 0;
