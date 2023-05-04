04b6ab5ae89d8fdc0acdfab56273b26dd94d5f89
Claudio Carvalho <cclaudio@linux.ibm.com>
2023-05-04 12:31:44 -0500

    psp/guest_request_cmd: Add send_request()
    As described in the SNP Firmware ABI specification, SNP_GUEST_REQUEST
    commands are protected using AES_GCM 256 and a VMPCK key to
    encrypt/decrypt the message. The firmware generates and provides the
    VMPCK keys to the guest in a especial secrets page that only the guest
    and firmware have access to, so the hypervisor cannot alter messages
    without detection nor read the plaintext of the message.
    
    We are limiting the send_request() implementation to encrypt/decrypt
    messages using the VMPCK0 key only. From guest userspace we can send
    SNP_GUEST_REQUEST commands using other VMPCK keys.
    
    The send_request() also supports sending extended SNP_GUEST_REQUEST
    commands, where the extended part of it is provided by the hypervisor.
    For example, in an extended attestation report the PSP firmware provides
    the attestation report and the hypervisor provides the certificate chain
    needed to verify the attestation report.
    
    If a SNP_GUEST_REQUEST fails, we disable the VMPCK0 to prevent it from
    being used again to send SNP_GUEST_REQUEST commands to the PSP firmware.
    The only two exceptions are when the hypervisor returns the error codes
    below:
    
    - BUSY: in this case we re-send the message and if it resturns any error
    code, we disable the VMPCK0.
    
    - INVALID_DATA_BUFFER_LEN: the data buffer provided in the extended
    SNP_GUEST_REQUEST command is too small. So, we re-send the command, but
    as non-extended SNP_GUEST_REQUEST in order to protect the IV (sequence
    number) from replay attacks. If it returns any error code, we disable
    the VMPCK0.
    
    The two cases above could be improved later when, for example, timer
    support is added to the SVSM.
    
    Signed-off-by: Claudio Carvalho <cclaudio@linux.ibm.com>

--- src/psp/guest_request_cmd.rs
+++ src/psp/guest_request_cmd.rs
@@ -5,11 +5,59 @@
  * Authors: Claudio Carvalho <cclaudio@linux.ibm.com>
  */
 
-use crate::mem::{mem_allocate, pgtable_make_pages_shared};
-use crate::{funcs, getter_func, prints, ALIGN, ALIGNED, PAGE_COUNT, PAGE_SHIFT, PAGE_SIZE};
+use crate::cpu::vc::vc_snp_guest_request;
+use crate::crypto::ssl::{
+    aes_256_gcm_decrypt, aes_256_gcm_encrypt, init_aes_256_gcm_ctx, new_ssl_ctx,
+};
+use crate::mem::snpsecrets::{disable_vmpck0, is_vmpck0_clear};
+use crate::mem::{mem_allocate, pgtable_make_pages_shared, SnpSecrets, VMPCK_SIZE};
+use crate::util::util::memset;
+use crate::{
+    funcs, get_svsm_secrets_page, getter_func, prints, ALIGN, ALIGNED, BIT, PAGE_COUNT, PAGE_SHIFT,
+    PAGE_SIZE,
+};
 
+use alloc::boxed::Box;
+use core::cmp::min;
+use core::ffi::c_int;
+use core::ptr;
+use core::ptr::copy_nonoverlapping;
+use core::sync::atomic::{AtomicU64, Ordering};
+use memoffset::offset_of;
 use x86_64::addr::VirtAddr;
 
+///
+/// AEAD Algorithm
+///
+
+/// 1
+const SNP_AEAD_AES_256_GCM: u8 = 1;
+
+///
+/// Hypervisor error codes
+///
+
+/// BIT!(32)
+pub const SNP_GUEST_REQ_INVALID_LEN: u64 = BIT!(32);
+/// BIT!(33)
+pub const SNP_GUEST_REQ_ERR_BUSY: u64 = BIT!(33);
+
+///
+/// SnpGuestRequestMsg
+///
+
+/// 1
+const HDR_VERSION: u8 = 1;
+/// 1
+const MSG_VERSION: u8 = 1;
+/// In the SEV-SNP ABI spec, the authentication tag should be at most
+/// 128 bits.
+/// 16
+const AUTHTAG_SIZE: c_int = 16;
+/// In the SEV-SNP ABI spec, the IV should be at most 96 bits; but
+/// the bits not used must be zeroed.
+/// 12
+const IV_SIZE: c_int = 12;
 /// 4000
 const MSG_PAYLOAD_SIZE: usize = 4000;
 
@@ -33,11 +81,95 @@ struct SnpGuestRequestMsgHdr {
     rsvd3: [u8; 35usize],
 }
 
+impl SnpGuestRequestMsgHdr {
+    pub fn is_valid(&self, msg_type: u8, msg_seqno: u64) -> bool {
+        const MSG_HDR_SIZE: usize = core::mem::size_of::<SnpGuestRequestMsgHdr>();
+
+        let header_size: u16 = match u16::try_from(MSG_HDR_SIZE) {
+            Ok(v) => v,
+            Err(_) => {
+                prints!("ERR: header size too big, {} bytes\n", MSG_HDR_SIZE);
+                return false;
+            }
+        };
+
+        // header version
+        if self.hdr_version != HDR_VERSION {
+            prints!(
+                "ERR: response header version {} should be {}\n",
+                { self.hdr_version },
+                HDR_VERSION
+            );
+            return false;
+        }
+
+        // header size
+        if self.hdr_sz != header_size {
+            prints!(
+                "ERR: response header size {} should be {}\n",
+                { self.hdr_sz },
+                header_size
+            );
+            return false;
+        }
+
+        // algo
+        if self.algo != SNP_AEAD_AES_256_GCM {
+            prints!(
+                "ERR: response algo {}, but should be {}\n",
+                { self.algo },
+                SNP_AEAD_AES_256_GCM
+            );
+            return false;
+        }
+
+        // message type
+        if self.msg_type != msg_type {
+            prints!(
+                "ERR: response message type {}, but should be {}\n",
+                { self.msg_type },
+                { msg_type }
+            );
+            return false;
+        }
+
+        // message vmpck
+        if self.msg_vmpck != 0 {
+            prints!("ERR: response message vmpck {}, but should be 0\n", {
+                self.msg_vmpck
+            });
+            return false;
+        }
+
+        // message sequence number
+        if self.msg_seqno != msg_seqno {
+            prints!(
+                "ERR: response message seqno {}, but should be {}\n",
+                { self.msg_seqno },
+                msg_seqno
+            );
+            return false;
+        }
+
+        true
+    }
+}
+
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
 struct SnpGuestRequestMsg {
     hdr: SnpGuestRequestMsgHdr,
     payload: [u8; MSG_PAYLOAD_SIZE],
+}
+
+static SEQ_NUM: AtomicU64 = AtomicU64::new(0);
+
+fn seqno_last_used() -> u64 {
+    SEQ_NUM.load(Ordering::Relaxed)
+}
+
+fn seqno_add_two() {
+    SEQ_NUM.fetch_add(2, Ordering::Relaxed);
 }
 
 pub struct SnpGuestRequestCmd {
@@ -107,5 +239,318 @@ impl SnpGuestRequestCmd {
         }
 
         Ok(())
+    }
+
+    fn set_ssl_ctx(&mut self, is_encryption: bool, msg_seqno: u64) -> Result<(), ()> {
+        self.ssl_ctx = new_ssl_ctx(self.ssl_ctx);
+        if self.ssl_ctx.is_null() {
+            prints!("ERR: Failed to create a new SSL context for encryption\n");
+            return Err(());
+        }
+
+        let svsm_secrets_ptr: *mut SnpSecrets = get_svsm_secrets_page().as_mut_ptr();
+        let vmpck0_va = VirtAddr::from_ptr(unsafe { *svsm_secrets_ptr }.vmpck0().as_ptr());
+        let vmpck0_len: c_int = match c_int::try_from(VMPCK_SIZE) {
+            Ok(c) => c,
+            Err(_) => {
+                prints!("ERR: VMPCK too big for c_int\n");
+                return Err(());
+            }
+        };
+        let mut msg_seqno_array: [u8; IV_SIZE as usize] = [0u8; IV_SIZE as usize];
+        const U64_SIZE: usize = core::mem::size_of::<u64>();
+        msg_seqno_array[..U64_SIZE].copy_from_slice(&u64::to_ne_bytes(msg_seqno));
+        let msg_seqno_array_va = VirtAddr::from_ptr(msg_seqno_array.as_ptr());
+
+        init_aes_256_gcm_ctx(
+            self.ssl_ctx,
+            is_encryption,
+            vmpck0_va,
+            vmpck0_len,
+            msg_seqno_array_va,
+            IV_SIZE,
+        )?;
+
+        Ok(())
+    }
+
+    /// Encrypt the plaintext using AES-256-GCM as described in the SNP ABI spec, where:
+    /// key = vmpck[0]
+    /// IV = sequence number
+    /// AAD = last 16 bytes of the SnpGuestRequestMsgHdr
+    fn encrypt_request(
+        &mut self,
+        msg_type: u8,
+        plaintext: VirtAddr,
+        plaintext_len: u16,
+    ) -> Result<(), ()> {
+        // Check VMPCK0 is valid
+        if is_vmpck0_clear() {
+            prints!("ERR: vmpck0 invalid\n");
+            return Err(());
+        }
+
+        // Clear the staging private page before using it for encrypting the request
+        memset(
+            self.staging_priv_page.as_mut_ptr::<u8>(),
+            0u8,
+            PAGE_SIZE as usize,
+        );
+
+        const MSG_HDR_SIZE: usize = core::mem::size_of::<SnpGuestRequestMsgHdr>();
+
+        // Construct the request message header
+        let req: *mut SnpGuestRequestMsg = self.staging_priv_page.as_mut_ptr();
+        let msg_seqno: u64 = match seqno_last_used().checked_add(1) {
+            Some(v) => v,
+            None => {
+                prints!("ERR: Request sequence number overflow\n");
+                return Err(());
+            }
+        };
+        unsafe {
+            (*req).hdr.hdr_sz = match u16::try_from(MSG_HDR_SIZE) {
+                Ok(v) => v,
+                Err(_) => {
+                    prints!("ERR: header size={} too big for u16\n", MSG_HDR_SIZE);
+                    return Err(());
+                }
+            };
+            (*req).hdr.algo = SNP_AEAD_AES_256_GCM;
+            (*req).hdr.hdr_version = HDR_VERSION;
+            (*req).hdr.msg_sz = plaintext_len;
+            (*req).hdr.msg_type = msg_type;
+            (*req).hdr.msg_version = MSG_VERSION;
+            (*req).hdr.msg_vmpck = 0;
+            (*req).hdr.msg_seqno = msg_seqno;
+        }
+
+        self.set_ssl_ctx(true, msg_seqno)?;
+
+        let algo_offset: c_int = match c_int::try_from(offset_of!(SnpGuestRequestMsgHdr, algo)) {
+            Ok(o) => o,
+            Err(_) => {
+                prints!("ERR: algo offset is too big for c_int\n");
+                return Err(());
+            }
+        };
+        let msg_hdr_size_cint: c_int = match c_int::try_from(MSG_HDR_SIZE) {
+            Ok(c) => c,
+            Err(_) => {
+                prints!("ERR: msg header size too big for c_int\n");
+                return Err(());
+            }
+        };
+        let aad_len: c_int = msg_hdr_size_cint - algo_offset;
+        let aad: VirtAddr = unsafe { VirtAddr::from_ptr(&(*req).hdr.algo) };
+        let ciphertext: VirtAddr = unsafe { VirtAddr::from_ptr((*req).payload.as_ptr()) };
+        let mut ciphertext_len: c_int = match c_int::try_from(MSG_PAYLOAD_SIZE) {
+            Ok(c) => c,
+            Err(_) => {
+                prints!("ERR: msg payload size too big for c_int\n");
+                return Err(());
+            }
+        };
+        let authtag: VirtAddr = unsafe { VirtAddr::from_ptr((*req).hdr.authtag.as_mut_ptr()) };
+
+        aes_256_gcm_encrypt(
+            self.ssl_ctx,
+            plaintext,
+            c_int::from(plaintext_len),
+            aad,
+            aad_len,
+            ciphertext,
+            ptr::addr_of_mut!(ciphertext_len),
+            authtag,
+            AUTHTAG_SIZE,
+        )?;
+
+        prints!(
+            "INFO: SNP_GUEST_REQUEST msg_type {} encrypted ({} bytes)\n",
+            msg_type,
+            ciphertext_len
+        );
+        //prints!("DEBUG: req_msg {:p} {:x?}\n", { &(*req) }, { *req });
+
+        memset(
+            self.req_shared_page.as_mut_ptr::<u8>(),
+            0u8,
+            PAGE_SIZE as usize,
+        );
+        unsafe {
+            copy_nonoverlapping(
+                self.staging_priv_page.as_ptr::<u8>(),
+                self.req_shared_page.as_mut_ptr::<u8>(),
+                PAGE_SIZE as usize,
+            );
+        }
+
+        Ok(())
+    }
+
+    /// Send the encrypted SNP_GUEST_REQUEST message to the PSP.
+    fn send(&mut self, extended: bool, mut psp_rc: &mut u64) -> Result<(), ()> {
+        memset(
+            self.resp_shared_page.as_mut_ptr::<u8>(),
+            0u8,
+            PAGE_SIZE as usize,
+        );
+
+        // Send the encrypted request
+        vc_snp_guest_request(extended, &mut psp_rc, self)?;
+
+        match *psp_rc {
+            // Success
+            0 => {}
+            // certs_buf too small, the hypervisor did not forward the request.
+            // Save the number of pages required for the certificate chain
+            // and send the request again as a non-extended request
+            // to prevent IV reuse.
+            SNP_GUEST_REQ_INVALID_LEN => {
+                if extended {
+                    let npages_required: usize = self.data_npages();
+                    vc_snp_guest_request(false, &mut psp_rc, self)?;
+                    self.set_data_npages(npages_required);
+                    if *psp_rc != 0 {
+                        return Err(());
+                    }
+                    *psp_rc = SNP_GUEST_REQ_INVALID_LEN;
+                }
+            }
+            // Hypervisor busy, the request was not forwarded to the PSP. Send
+            // the request again to prevent IV reuse.
+            SNP_GUEST_REQ_ERR_BUSY => {
+                vc_snp_guest_request(extended, &mut psp_rc, self)?;
+                if *psp_rc != 0 {
+                    return Err(());
+                }
+            }
+            // Failed. See the status codes in the SEV SNP ABI spec or in the
+            // linux kernel include/uapi/linux/psp-sev.h
+            _ => {
+                prints!("ERR: SNP_GUEST_REQUEST failed, rc={}\n", { *psp_rc });
+                return Err(());
+            }
+        }
+
+        // The PSP firmware increases the sequence number only when
+        // it receives a request successfully. Hence, we sync our
+        // sequence number (add two) only when we receive a response
+        // successfully.
+        seqno_add_two();
+
+        Ok(())
+    }
+
+    fn decrypt_response(&mut self, msg_type: u8) -> Result<Box<[u8]>, ()> {
+        // Check VMPCK0 is valid
+        if is_vmpck0_clear() {
+            prints!("ERR: vmpck0 invalid\n");
+            return Err(());
+        }
+
+        // Decrypt the response in a private page to avoid any interference from
+        // the hypervisor
+        memset(
+            self.staging_priv_page.as_mut_ptr::<u8>(),
+            0u8,
+            PAGE_SIZE as usize,
+        );
+        unsafe {
+            copy_nonoverlapping(
+                self.resp_shared_page.as_ptr::<u8>(),
+                self.staging_priv_page.as_mut_ptr::<u8>(),
+                PAGE_SIZE as usize,
+            );
+        }
+
+        let resp: *const SnpGuestRequestMsg = self.staging_priv_page.as_ptr();
+
+        unsafe {
+            // Check if the response header is valid
+            if !(*resp).hdr.is_valid(msg_type, seqno_last_used()) {
+                return Err(());
+            }
+        }
+
+        self.set_ssl_ctx(false, seqno_last_used())?;
+
+        let mut plaintext: [u8; MSG_PAYLOAD_SIZE] = [0u8; MSG_PAYLOAD_SIZE];
+        let mut plaintext_len: c_int = match c_int::try_from(MSG_PAYLOAD_SIZE) {
+            Ok(c) => c,
+            Err(_) => {
+                prints!("ERR: msg payload size too big for c_int\n");
+                return Err(());
+            }
+        };
+        let algo_offset: c_int = match c_int::try_from(offset_of!(SnpGuestRequestMsgHdr, algo)) {
+            Ok(o) => o,
+            Err(_) => {
+                prints!("ERR: algo offset is too big for c_int\n");
+                return Err(());
+            }
+        };
+        const MSG_HDR_SIZE: usize = core::mem::size_of::<SnpGuestRequestMsgHdr>();
+        let __msg_hdr_size: c_int = match c_int::try_from(MSG_HDR_SIZE) {
+            Ok(c) => c,
+            Err(_) => {
+                prints!("ERR: msg header size is too big for c_int\n");
+                return Err(());
+            }
+        };
+        let aad_len: c_int = __msg_hdr_size - algo_offset;
+        let aad: VirtAddr = unsafe { VirtAddr::from_ptr(&(*resp).hdr.algo) };
+        let ciphertext: VirtAddr = unsafe { VirtAddr::from_ptr((*resp).payload.as_ptr()) };
+        let ciphertext_len: c_int = unsafe { c_int::from((*resp).hdr.msg_sz) };
+        let authtag: VirtAddr = unsafe { VirtAddr::from_ptr((*resp).hdr.authtag.as_ptr()) };
+
+        aes_256_gcm_decrypt(
+            self.ssl_ctx,
+            VirtAddr::from_ptr(plaintext.as_mut_ptr()),
+            ptr::addr_of_mut!(plaintext_len),
+            aad,
+            aad_len,
+            ciphertext,
+            ciphertext_len,
+            authtag,
+            AUTHTAG_SIZE,
+        )?;
+
+        prints!(
+            "INFO: SNP_GUEST_REQUEST msg_type {} decrypted ({} bytes)\n",
+            msg_type,
+            plaintext_len
+        );
+        //prints!("DEBUG: resp_msg {:x?}\n", { &buf[..500] });
+
+        Ok(plaintext.into())
+    }
+
+    /// Send a SNP_GUEST_REQUEST message to the platform security processor (PSP) following
+    /// the GHCB protocol. Messages are a encrypted/decrypted using AES_GCM.
+    pub fn send_request(
+        &mut self,
+        msg_type: u8,
+        extended: bool,
+        payload: VirtAddr,
+        payload_size: u16,
+        psp_rc: &mut u64,
+    ) -> Result<Box<[u8]>, ()> {
+        if !self.is_initialized {
+            return Err(());
+        }
+        self.encrypt_request(msg_type, payload, payload_size)?;
+
+        if self.send(extended, psp_rc).is_err() {
+            disable_vmpck0();
+            return Err(());
+        }
+
+        let result: Result<Box<[u8]>, ()> = self.decrypt_response(msg_type + 1);
+        if result.is_err() {
+            disable_vmpck0();
+        }
+
+        result
     }
 }
